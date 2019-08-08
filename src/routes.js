5554389d164a914eefec2fca7e645401262483ae
Manoel Carvalho <manoelmecat@gmail.com>
2019-08-08 18:23:15 -0300

    Fix Now

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,6 @@ routes.get('/', (req, res) => {
 routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
 routes.post('/devs/:devId/likes', LikeController.store)
-routes.post('/devs/:devId/deslikes', DislikeController.store)
+routes.post('/devs/:devId/dislikes', DislikeController.store)
 
 module.exports = routes
